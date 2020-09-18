05ea62e92a7d79d1b66e0977ac5b5a3e994426d2
Matheus Henrique Conceição Bonore <matheus.bonore@gmail.com>
2020-09-17 22:51:50 -0300

    first commit

---
+++ Assets/Scripts/CubeEx3Behaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CubeEx3Behaviour : MonoBehaviour
+{
+    private bool mouseInObject = false;
+
+    public AudioSource audioBeep;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseInObject && Input.GetMouseButtonDown(0))
+            audioBeep.Play();
+    }
+
+    void OnMouseEnter()
+    {
+        mouseInObject = !mouseInObject;
+    }
+
+    void OnMouseExit()
+    {
+        mouseInObject = !mouseInObject;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (transform.position.x > 20)
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+        if (transform.position.x < -0)
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+
+        if (transform.position.z > 20)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 20);
+        if (transform.position.z < -0)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+    }
+}
