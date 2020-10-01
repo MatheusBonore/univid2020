80c7c59cf46b2ff3148b7b811ccccb38727d6144
Henrique T. Kato <henrikato@MacBook-Pro-do-Kato.local>
2020-09-30 23:35:04 -0300

    Script para o objeto que vai atirar projeteis

---
+++ Assets/Atirar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Atirar : MonoBehaviour
+{
+    public GameObject projetil;
+    public int velocidade = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameObject projetil_inst = Instantiate(projetil, transform.position, Quaternion.identity);
+            Rigidbody projetil_inst_rigidbody = projetil_inst.GetComponent<Rigidbody>();
+            projetil_inst_rigidbody.AddForce(Vector3.forward * velocidade);
+        }
+    }
+}
