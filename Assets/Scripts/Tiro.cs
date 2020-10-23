ff376d6f53d4e9d47a74c65e4e7d82eddf4ce7d0
henrikato <henrique.kato2@outlook.com>
2020-10-23 20:42:50 -0300

    Corrigindo Zumbi e script de Tiro
    Corrigindo script de tiro para deixar apenas scripts para VR e adicionando mesh faltando no Zumbi

--- Assets/Scripts/Tiro.cs
+++ Assets/Scripts/Tiro.cs
@@ -21,29 +21,11 @@ public class Tiro : MonoBehaviour
         vrInput.OnClick -= Disparar;
     }
 
-    //APENAS PARA TESTE NO UNITY
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100))
-            {
-                Inimigo inimigo = hit.transform.gameObject.GetComponentInParent<Inimigo>();
-                if (inimigo == null) return;
-
-                //TODO tocar som
-                //TODO animacao de tiro
-
-                inimigo.SendMessage("HitTarget", dano);
-            }
-        }
-    }
-
     public void Disparar()
     {
-        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.position, out RaycastHit hit, 100))
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100))
         {
-            Inimigo inimigo = hit.transform.GetComponent<Inimigo>();
+            Inimigo inimigo = hit.transform.gameObject.GetComponentInParent<Inimigo>();
             if (inimigo == null) return;
 
             //TODO tocar som
